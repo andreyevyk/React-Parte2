60ae660946c138f5fd95d6cd5aeeda3be2875d5d
Andrey Evyk <andreyevyk13@gmail.com>
2020-01-09 00:40:20 -0300

    Corrigindo DataTable

--- src/DataTable.js
+++ src/DataTable.js
@@ -1,15 +1,13 @@
 import React from "react";
 
 const DataTable = props =>{
-   console.log(props);
-   console.log(props.colunas);
-   let linhas = props.dados.map(item => 
-      <tr key={item.id}>
-         {props.colunas.map(coluna => 
-            <td key={`${item.id}${item[coluna]}`}> {item[coluna]} </td>
+   let linhas = props.dados.map((item,index) => 
+      <tr key={index}>
+         {props.colunas.map((coluna) => 
+               <td key={`${index}${item[coluna]}`}>{item[coluna]}</td>
          )}
       </tr>
-   )
+   );
    
    return(
       <table>
--- src/Livros.js
+++ src/Livros.js
@@ -1,13 +1,47 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, Component} from 'react'
 import Header from './Header'
+import DataTable from './DataTable'
 
-const Livros = () =>{
-   return (
-      <Fragment>
-         <Header/>
-         <h1>Casa do código</h1>      
-      </Fragment>
-   );
-   
+class Livros extends Component{
+   constructor(props){
+      super(props);
+
+      this.state = {
+         titulo: 'Livros',
+         autores: [
+           {
+             nome: 'Paulo',
+             livro: 'React',
+             preco: '1000'
+           },
+           {
+             nome: 'Daniel',
+             livro: 'Java',
+             preco: '99'
+           },
+           {
+             nome: 'Marcos',
+             livro: 'Design',
+             preco: '150'
+           },
+           {
+             nome: 'Bruno',
+             livro: 'DevOps',
+             preco: '100'
+           }
+         ],
+      }
+   }
+
+   render() {
+      return (
+         <Fragment>
+            <Header/>
+            <div className="container">
+               <DataTable dados={this.state.autores} titulo={this.state.titulo} colunas={['livro']}/>
+            </div>
+         </Fragment>
+      );
+   }
 }
 export default Livros;
